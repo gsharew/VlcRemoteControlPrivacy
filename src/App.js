c916b1d7031fae1cc1787928c2f7c105a7c49c60
gsharew <gsharewz@gmail.com>
2022-09-16 09:38:42 +0100

    gh-pages file commited

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from './Components/js/Header.js';
+import Body from './Components/js/Body.js';
+
+function App() {
+  return (
+    <div className="App">
+       <Header/>
+       <Body/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/js/Body.js
@@ -0,0 +1,25 @@
+import header from '../css/header.css';
+const Body = () =>{
+     return(
+        <div className='centeredDiv'>
+            <p><b>Vlc Remote Control Privacy Policy</b></p>
+            <label>This privacy policy governs your use of the software application vlc remote control for mobile devices. The Application is a utility application designed to control vlc remotely. What information does the Application obtain and how is it used? Does the Application collect precise real time location information of the device? This Application does not collect precise information about the location of your mobile device.</label>
+            <p><b>Do third parties see and/or have access to information obtained by the Application?</b></p>
+            <label>No, as we are not collecting any of your information.</label>
+            <p><b>Why we need access to your internal storage?</b></p>
+            <label>When you want to stream your internal storage musics and videos to remote VLC we need to access your internal storage directories</label>
+            <p><b>Why we need access to your network?</b></p>
+            <label>This is because Controlling VLC remotely needs a network connection</label>
+            <p><b>Automatic Data Collection and Advertising</b></p>
+            <label>This application does never collect your data as well as it has no Advertising</label>
+            <p><b>Changes</b></p>
+            <label>This Privacy Policy may be updated from time to time for any reason. We will notify you of any changes to our Privacy Policy by posting the new Privacy Policy here .</label>
+            <p><b>Your Concent</b></p>
+            <label>By using the Application, you are consenting to our access of your information as set forth in this Privacy Policy now and as amended by us.</label>
+            <p><b>Contact Us</b></p>
+            <label>If you have any questions regarding privacy while using the Application, or have questions about our practices, please contact us via email at <i>gsharewz@gmail.com</i></label>
+        </div>
+     )
+}
+
+export default Body;
